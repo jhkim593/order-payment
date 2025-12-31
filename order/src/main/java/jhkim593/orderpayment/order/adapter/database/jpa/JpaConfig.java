939a898668f71b4a111e18879340f50583925c9c
jhkim593 <jhkim593@sparrow.im>
2025-12-31 17:09:42 +0900

    chore: jpa coonfig  수정

--- order/src/main/java/jhkim593/orderpayment/order/adapter/database/jpa/JpaConfig.java
+++ order/src/main/java/jhkim593/orderpayment/order/adapter/database/jpa/JpaConfig.java
@@ -3,10 +3,14 @@ package jhkim593.orderpayment.order.adapter.database.jpa;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "jhkim593.orderpayment.order.*")
+@EntityScan(basePackages = "jhkim593.orderpayment.order.*")
 public class JpaConfig {
     @PersistenceContext
     private EntityManager entityManager;
